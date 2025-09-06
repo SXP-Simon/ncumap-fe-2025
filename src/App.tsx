83726bce1afa95ab127927f9f60886314c1b4c9e
回归天空 <sxp20061207@163.com>
2025-09-06 11:13:14 +0800

    [hero] (hero-ui) 初始化 hero ui 的配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Detail from './pages/Detail';
-import './App.css';
 import 'ol/ol.css';
+import {HeroUIProvider} from "@heroui/react";
+
 
 function App() {
   return (
-    <Router>
+    <HeroUIProvider>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/:id" element={<Detail />} />
       </Routes>
-    </Router>
+    </HeroUIProvider>
   );
 }
 
