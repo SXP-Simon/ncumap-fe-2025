a2920eac33a4344a7e9e3e61d84c0b4d70f3795e
回归天空 <sxp20061207@163.com>
2025-09-08 17:15:57 +0800

    [remove] (BottomSheet) 删除已经弃用的组件

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,4 @@
 export { NavigationTabs } from './NavigationTabs';
 export { SchoolCarModal } from './SchoolCarModal';
-export { BottomSheet } from './BottomSheet';
 export { GlassmorphismSelectingSheet } from './GlassmorphismSelectingSheet';
 export { FloatingActionButtons } from './FloatingActionButtons';
